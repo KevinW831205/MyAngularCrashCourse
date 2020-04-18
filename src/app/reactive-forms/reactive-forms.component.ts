5550cb106dfc80f37db1357b22216a13d1442fa8
KevinW <bird831205@hotmail.com>
2020-04-17 21:18:57 -0400

    updating the form builder version

--- src/app/reactive-forms/reactive-forms.component.ts
+++ src/app/reactive-forms/reactive-forms.component.ts
@@ -9,22 +9,26 @@ import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms'
 export class ReactiveFormsComponent implements OnInit {
 
   //manual creating the form group
-  form = new FormGroup({
-    username: new FormControl('', [Validators.required]),
-    password: new FormControl(''),
-    contactInfo: new FormGroup({
-      email: new FormControl('intialized Value'),
-      phone: new FormControl('')
-    })
-  })
+  // form = new FormGroup({
+  //   username: new FormControl('', [Validators.required]),
+  //   password: new FormControl(''),
+  //   contactInfo: new FormGroup({
+  //     email: new FormControl('intialized Value'),
+  //     phone: new FormControl('')
+  //   })
+  // })
 
-  // form: FormGroup;    // with form builder
+  form: FormGroup;    // with form builder
 
   constructor(fb: FormBuilder) {
-    // this.form = fb.group({
-    //   username: ['',Validators.required],
-    //   password: ['']
-    // })
+    this.form = fb.group({
+      username: ['',Validators.required],
+      password: [''],
+      contactInfo: fb.group({
+        email: ['initialzied Value'],
+        phone: ['']
+      })
+    })
 
   }
 

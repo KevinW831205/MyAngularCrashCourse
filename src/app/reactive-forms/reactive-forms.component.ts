98975e589af12d2f58cb90e1a3aacee55cf87f30
KevinW <bird831205@hotmail.com>
2020-04-17 21:17:40 -0400

    nested Form groups

--- src/app/reactive-forms/reactive-forms.component.ts
+++ src/app/reactive-forms/reactive-forms.component.ts
@@ -9,19 +9,22 @@ import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms'
 export class ReactiveFormsComponent implements OnInit {
 
   //manual creating the form group
-  // form = new FormGroup({
-  //   username: new FormControl('', [Validators.required]),
-  //   password: new FormControl(''),
+  form = new FormGroup({
+    username: new FormControl('', [Validators.required]),
+    password: new FormControl(''),
+    contactInfo: new FormGroup({
+      email: new FormControl('intialized Value'),
+      phone: new FormControl('')
+    })
+  })
 
-  // })
-
-  form: FormGroup;    // with form builder
+  // form: FormGroup;    // with form builder
 
   constructor(fb: FormBuilder) {
-    this.form = fb.group({
-      username: ['',Validators.required],
-      password: ['']
-    })
+    // this.form = fb.group({
+    //   username: ['',Validators.required],
+    //   password: ['']
+    // })
 
   }
 
